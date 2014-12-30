84636108656242c66b49e2e0e379e5f0428ede3c
Jackson Kearl <jkearl11@gmail.com>
2014-12-30 14:58:36 -0800

    One Node Shell

--- FreeBody/OneBodyScene.swift
+++ FreeBody/OneBodyScene.swift
@@ -9,5 +9,83 @@
 import SpriteKit
 
 class OneBodyScene: SKScene {
-   
+    let node:SKShapeNode?
+    let options:SKShapeNode?
+    var isOptionVisible:Bool = false
+
+    func UIColorFromRGB(rgbValue: UInt) -> UIColor {
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+
+    override func didMoveToView(view: SKView) {
+        self.backgroundColor = UIColorFromRGB(0x455E98)
+    }
+
+    override init(size: CGSize) {
+        super.init(size: size)
+        node = SKShapeNode(rectOfSize: CGSizeMake(self.size.width/4, self.size.width/4));
+        node!.fillColor = UIColorFromRGB(0xE3CB58)
+        node!.lineWidth = 0
+        node!.name = "Node"
+
+        node!.position = CGPointMake(self.size.width/2, self.size.height/2)
+        self.addChild(node!)
+
+        options = SKShapeNode(rectOfSize: CGSizeMake(self.size.width/3, self.size.height))
+        options!.fillColor = UIColorFromRGB(0xD49A6A)
+        options!.position = CGPointMake(self.size.width*7/6, self.size.height/2)
+        options!.name = "Options"
+        options!.lineWidth = 0
+        self.addChild(options!)
+
+        self.name = "Background"
+
+
+
+    }
+
+    func showOptionPane() {
+        if !isOptionVisible {
+            for child in children {
+                (child as SKNode).runAction(SKAction.moveBy(CGVectorMake(-self.frame.width/3, 0), duration: 0.25))
+            }
+            isOptionVisible = true
+        }
+    }
+
+    func hideOptionPane() {
+        if isOptionVisible {
+            for child in children {
+                (child as SKNode).runAction(SKAction.moveBy(CGVectorMake(+self.frame.width/3, 0), duration: 0.25))
+            }
+            isOptionVisible = false
+        }
+    }
+
+    override func touchesEnded(touches: NSSet, withEvent event: UIEvent) {
+        let touch = touches.anyObject() as UITouch
+        let nodeTouched = nodeAtPoint(touch.locationInNode(self))
+        if (nodeTouched.name? != nil) {
+            switch nodeTouched.name! {
+            case "Node":
+                showOptionPane()
+            case "Options":
+                print()
+            case "Background":
+                hideOptionPane()
+            default:
+                println("Nothing Touched")
+            }
+        }
+
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
